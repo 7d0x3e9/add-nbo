d53368ec4c3f826218132ae8c3e876cac98d6dcf
zeze <121916836+7d0x3e9@users.noreply.github.com>
2023-07-15 06:26:54 +0900

    Update add-nbo.cpp

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -4,13 +4,13 @@
 #include <arpa/inet.h> // for ntohl
 
 uint32_t read_network_order_integer(const char* file_path) {
-    FILE *file = fopen(file_path, "rb");
+    FILE *file = fopen(file_path, "rb"); // 파일 오픈
     if (file == NULL) {
-        perror("Failed to open file");
+        perror("file open failed"); // 파일 오픈 실패 시
         exit(1);
     }
 
-    uint32_t num;
+    uint32_t num; // 16진수 계산
     if (fread(&num, sizeof(uint32_t), 1, file) != 1) {
         perror("Failed to read data from file");
         exit(1);
@@ -18,17 +18,17 @@ uint32_t read_network_order_integer(const char* file_path) {
 
     fclose(file);
 
-    return ntohl(num); // convert from network byte order to host byte order
+    return ntohl(num); // N to H, Network byte order 를 host by order로 변환
 }
 
 int main() {
-    const char* file_path1 = "a.bin"; // replace with your actual file paths
-    const char* file_path2 = "b.bin";
+    const char* file_path1 = "a.bin"; // 계산할 파일 설정
+    const char* file_path2 = "b.bin"; // 계산할 파일 설정
 
-    uint32_t number1 = read_network_order_integer(file_path1);
-    uint32_t number2 = read_network_order_integer(file_path2);
+    uint32_t number1 = read_network_order_integer(file_path1); // 파일 내용 불러오기
+    uint32_t number2 = read_network_order_integer(file_path2); // 파일 내용 불러오기
 
-    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", number1, number1, number2, number2, number1 + number2, number1 + number2);
+    printf("%d(0x%x) + %d(0x%x) = %d(0x%x)\n", number1, number1, number2, number2, number1 + number2, number1 + number2); // 최종 결과 출력
 
     return 0;
 }
